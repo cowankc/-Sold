7cd64857becd1010d4c7008e1c9cabdbcda6c63c
Lorena Perales <lperales321@gmail.com>
2019-11-04 14:36:34 -0600

    Add routes for users, meals, reviews

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,19 +3,28 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Meal.findAll({}).then(function(dbMeals) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        meals: dbMeals
       });
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  // Load meal page by id
+  app.get("/meal/:id", function(req, res) {
+    db.Meal.findOne({ where: { id: req.params.id } }).then(function(dbMeals) {
+      res.render("meal", {
+        meal: dbMeals
+      });
+    });
+  });
+
+  // Load review page by chef id
+  app.get("/review/:chefId", function(req, res) {
+    db.Review.findOne({ where: { id: req.params.chefId } }).then(function(dbReviews) {
+      res.render("review", {
+        review: dbReviews
       });
     });
   });
