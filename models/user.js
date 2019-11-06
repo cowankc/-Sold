e403b08272fb559652f89dd3a7681082af5b86e0
cowankc <cowankc92@live.com>
2019-11-05 19:37:18 -0600

    added models files back in

--- models/user.js
+++ models/user.js
@@ -1,34 +1,34 @@
 module.export = function(sequelize, DataTypes) {
-let User = sequelize. define("user", {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      min: 1
-    }
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      min: 1 
-    }
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      min: 6 
-    }
-  },
-  freezeTableName: true
-});
-  user.associate = function(models) {
-    user.hasMany(models.review, {
-      foreignKey: {
-        allowNull: false
+  let User = sequelize. define("user", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        min: 1
       }
-    });
-  };
-return User
-}
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        min: 1 
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        min: 6 
+      }
+    },
+    freezeTableName: true
+  });
+    user.associate = function(models) {
+      user.hasMany(models.review, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  return User
+  }
