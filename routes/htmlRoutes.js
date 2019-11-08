e6926c75b3c718c9b574383a46e1872ff31b8ae1
cowankc <cowankc92@live.com>
2019-11-07 18:49:35 -0600

    added previous html and api routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,32 +4,12 @@ module.exports = function(app) {
   // Get all meals and Route to home page 
   app.get("/", function(req, res) {
     db.meal.findAll({}).then(function(dbMeals) {
-      console.log(dbMeals[0].dataValues);
       res.render("homepage", {
         meals: dbMeals
       });
     });
   });
 
-  // Load mela page
-  app.get("/mealpage", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("mealpage", {
-        msg: "meal page!",
-        examples: dbExamples
-      });
-    });
-  });
-  
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
   // Search by meal name and route to meal page
   app.get("/meal/:name", function(req, res) {
     db.meal.findOne({ where: { mealName: req.params.name } }).then(function(dbMeal) {
