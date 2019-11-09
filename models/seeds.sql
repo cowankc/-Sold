292762e913542e8d051c51332eb2cc9226a8e104
cowankc <cowankc92@live.com>
2019-11-09 11:42:32 -0600

    added the photo column to meal model

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,10 +1,49 @@
-INSERT INTO chef (name, email, password) VALUES ('Tom Johnson','johnsonboy@yahoo.com', 'password123');
+INSERT INTO users (name, email, password, chef) VALUES ('Tom Johnson','johnsonboy@yahoo.com', 'password123', 1);
 
-INSERT INTO meals (mealName, ingredients, price, address, category, chefId) 
-VALUES ('Cowboy Burger', 'ground meat, garlic, salt, pepper, grilled onions, BBQ sauce, pepperjack Cheese, bun', 3, '4510 W Guadalupe St, Austin, TX 78751', 'American', 1);
-INSERT INTO meals (mealName, ingredients, price, address, category, chefId) 
+INSERT INTO meals (mealName, photo, ingredients, price, address, category) 
+VALUES (
+'Cowboy Burger', 
+'https://www.tastefullysimple.com/_/media/images/recipes/cowboyburger.jpg',
+'ground meat, garlic, salt, pepper, grilled onions, BBQ sauce, pepperjack Cheese, bun',
+ 3, 
+ '4510 W Guadalupe St, Austin, TX 78751', 
+ 'American', 
+ 1);
+ 
+INSERT INTO meals (mealName, photo, ingredients, price, address, category) 
 VALUES ('beef fajitas', 'skirt steak, garlic, salt, pepper, cumin, chili powder, grilled peppers + onions, chile sauce, shredded cheese, tortilla', 1.50, '4510 W Guadalupe St, Austin, TX 78751', 'American', 1);
-INSERT INTO meals (mealName, ingredients, price, address, category, chefId) 
+INSERT INTO meals (mealName, photo, ingredients, price, address, category) 
 VALUES ('Grilled Chicken Kabobs', 'Chicken, onions, pepers, lime, tomato paste', 2, '4510 W Guadalupe St, Austin, TX 78751', 'American', 1);
 
-INSERT INTO chef (name, email, password) VALUES ('Giovanni Romano','gioguy@yahoo.com', 'password123');
+
+INSERT INTO users (name, email, password, chef) VALUES ('Giovanni Romano','gioguy@yahoo.com', 'password123', 1);
+
+INSERT INTO meals (mealName, photo, ingredients, price, address, category) 
+VALUES ('Grilled Chicken Risotto', 
+'https://www.thespruceeats.com/thmb/OT9C15ZzeZETdJ6bxYCxEMIDnYc=/2500x1669/filters:fill(auto,1)/chickenrisotto2500-598cd29b6f53ba001014cb54.jpg',
+'Chicken, onions, rice, wine, chicken stock, parmesean',
+ 3, 
+ '2504 New York Dr, Austin, TX 78702'
+ , 'mediterranean',
+1);
+
+INSERT INTO meals (mealName, photo, ingredients, price, address, category) 
+VALUES (
+'Pasta bolognese', 
+'https://www.jocooks.com/wp-content/uploads/2018/01/spaghetti-bolognese-1-3.jpg',
+'Ground meat, onions, tomatos, wine, garlic, parmesean, pasta', 
+2, 
+'2504 New York Dr, Austin, TX 78702', 
+'mediterranean', 
+1);
+
+INSERT INTO meals (mealName, photo, ingredients, price, address, category) 
+VALUES (
+'fettuccine alfredo with Chicken', 
+'https://www.modernhoney.com/wp-content/uploads/2019/04/Chicken-Fettucine-Alfredo-6.jpg',
+'Chicken, fettuccine, cream, pepper, garlic, 
+parmesean', 
+3, 
+'2504 New York Dr, Austin, TX 78702', 
+'mediterranean', 
+1);
